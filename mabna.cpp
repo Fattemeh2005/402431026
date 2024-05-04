972c0077dff5a63325554ae838ecd92bb591daed
Fatemeh402431026 <162762857+Fattemeh2005@users.noreply.github.com>
2024-05-04 19:48:02 +0330

    mabna.cpp

---
+++ mabna.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cin >> x;
+    while (x / 2 != 0)
+    {
+        cout << x % 2;
+        x = x / 2;
+    }
+    cout << x % 2;
+}
